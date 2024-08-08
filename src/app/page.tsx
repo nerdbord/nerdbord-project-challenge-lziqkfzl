546595d1185160be228082ef382271e218e86695
AniaPiWo <snowflake@wp.pl>
2024-08-08 11:52:41 +0200

    feat: new user prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,8 @@
-"use client";
+import { checkUserInDatabase } from "@/actions/user";
 
-import {
-  generateTextAction,
-  describeImage,
-  generateJoke,
-} from "./actions/form";
-import { useState } from "react";
+export default async function Page() {
+  const prisma_user = await checkUserInDatabase();
+  console.log(prisma_user);
 
-export default function Page() {
-  const [generation, setGeneration] = useState("");
-  const [image, setImage] = useState("");
-  const [object, setObject] = useState({});
-  return (
-    <div className="space-y-4">
-      <div>
-        <button
-          onClick={async () => {
-            const result = await generateTextAction();
-            setGeneration(result);
-          }}
-        >
-          tell me a joke
-        </button>
-        <pre>{JSON.stringify(generation, null, 2)}</pre>
-      </div>
-      <div>
-        <button
-          onClick={async () => {
-            const result = await describeImage();
-            setImage(result);
-          }}
-        >
-          describe image
-        </button>
-        <pre>{JSON.stringify(image, null, 2)}</pre>
-      </div>
-    </div>
-  );
+  return <div className="space-y-4">hello</div>;
 }
-/* https://www.youtube.com/watch?v=UDm-hvwpzBI */
---
+++ src/app/sdk/page.tsx
@@ -0,0 +1,35 @@
+import { generateTextAction, describeImage } from "@/actions/form";
+import { useState } from "react";
+
+export default async function Page() {
+  const [generation, setGeneration] = useState("");
+  const [image, setImage] = useState("");
+  const [object, setObject] = useState({});
+  return (
+    <div className="space-y-4">
+      <div>
+        <button
+          onClick={async () => {
+            const result = await generateTextAction();
+            setGeneration(result);
+          }}
+        >
+          tell me a joke
+        </button>
+        <pre>{JSON.stringify(generation, null, 2)}</pre>
+      </div>
+      <div>
+        <button
+          onClick={async () => {
+            const result = await describeImage();
+            setImage(result);
+          }}
+        >
+          describe image
+        </button>
+        <pre>{JSON.stringify(image, null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
+/* https://www.youtube.com/watch?v=UDm-hvwpzBI */
