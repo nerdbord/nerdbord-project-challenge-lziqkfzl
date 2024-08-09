1ed1bde1d7e3d585410a8396f96ca556906911a4
AniaPiWo <snowflake@wp.pl>
2024-08-09 13:18:57 +0200

    feat: landing

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/lib/prisma";
 export async function checkUserInDatabase() {
   try {
     const user = await currentUser();
+    if (!user) return "No user found";
     if (user) {
       let existingUser = await prisma.user.findUnique({
         where: { email: user.emailAddresses[0].emailAddress },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { checkUserInDatabase } from "@/actions/user";
+import { currentUser } from "@clerk/nextjs/server";
 import { FormGenerator } from "@/components/FormGenerator";
+import Landing from "@/components/Landing";
 
 export default async function Page() {
+  const user = await currentUser();
   const prisma_user = await checkUserInDatabase();
 
+  console.log(prisma_user);
+
   return (
-    <div className="space-y-4">
-      <FormGenerator />
-    </div>
+    <div className="space-y-4">{user ? <FormGenerator /> : <Landing />}</div>
   );
 }
